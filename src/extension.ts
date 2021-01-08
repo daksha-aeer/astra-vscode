4746bc71d12df975f9073a0a01b868d17758e434
shardul <shardul.aeer@somaiya.edu>
2021-01-08 21:56:54 +0530

    feat: Accept service credentials from user

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,37 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { NodeDependenciesProvider } from './Provider';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Starting Astra extension');
 
 	const provider = new NodeDependenciesProvider(vscode.workspace.rootPath);
 	vscode.window.registerTreeDataProvider(
-		'astra',
+		'astra-explorer',
 		provider
 	);
 
-	let disposable = vscode.commands.registerCommand('astra-vscode.helloWorld', () => {
+	vscode.commands.registerCommand('astra-vscode.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from Astra VSCode!');
+	});
+
+	vscode.commands.registerCommand('astra-vscode.readServiceCredentials', async () => {
+		console.log('Reading service credentials')
+		const sampleCredentials = { clientId: "your-id", clientName: "user@domain.com", clientSecret: "secret" }
+		const userInput = await vscode.window.showInputBox({
+			prompt: 'Paste the credentials in the above format.',
+			placeHolder: JSON.stringify(sampleCredentials),
+		})
+		try {
+			const serviceCredentials: typeof sampleCredentials = JSON.parse(userInput!);
+			console.log('Got', serviceCredentials);
+		} catch (error) {
+			vscode.window.showErrorMessage('Invalid credentials!')
+		}
 	});
 
 	vscode.commands.registerCommand('astra-vscode.refreshEntry', () => {
 		provider.refresh();
 	});
-
-	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { }
