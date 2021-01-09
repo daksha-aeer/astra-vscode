a58ddc9e81cb15868e7a4571f5c5e56f58178a45
shardul <shardul.aeer@somaiya.edu>
2021-01-09 13:20:46 +0530

    feat: Fetch database list

--- src/extension.ts
+++ src/extension.ts
@@ -17,38 +17,49 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from Astra VSCode!');
 	});
 
-	vscode.commands.registerCommand('astra-vscode.readServiceCredentials', async () => {
-		console.log('Reading service credentials')
+	vscode.commands.registerCommand('astra-vscode.setServiceCredentials', async () => {
+		console.log('Setting service credentials')
 
 		const userInput = await vscode.window.showInputBox({
 			prompt: 'Paste the credentials in the above format.',
 			placeHolder: JSON.stringify(sampleCredentials),
 		})
 		await context.globalState.update('serviceCredentials', userInput);
+
+		vscode.commands.executeCommand('astra-vscode.refreshDevOpsToken');
 	});
 
-	vscode.commands.registerCommand('astra-vscode.connectAstraDevOpsApi', async () => {
+	vscode.commands.registerCommand('astra-vscode.refreshDevOpsToken', async () => {
+		console.log('Refreshing token');
 		const serviceCredentials = context.globalState.get<string>('serviceCredentials');
 		console.log('Retrieved credentials', serviceCredentials);
 
 		try {
 			const result = await fetch('https://api.astra.datastax.com/v2/authenticateServiceAccount', {
 				method: 'POST',
-				headers: {
-					Accept: "application/json",
-					'Content-Type': 'application/json'
-				},
 				body: serviceCredentials,
 			}).then(res => res.json());
 
 			if (result.token === null) {
 				throw 'Failed to get token';
+			} else {
+				vscode.window.showInformationMessage('Token refreshed');
+				devOpsToken = result.token;
+				console.log('Got devOps token', devOpsToken);
 			}
-			devOpsToken = result.token;
-			console.log('Got devOps token', devOpsToken);
 		} catch (error) {
 			console.error(error);
 		}
+	})
+
+	vscode.commands.registerCommand('astra-vscode.refreshUserDatabases', async () => {
+		console.log('Token for fetching databases', devOpsToken);
+		const result = await fetch('https://api.astra.datastax.com/v2/databases?include=nonterminated', {
+			headers: {
+				Authorization: "Bearer " + devOpsToken,
+			}
+		}).then(res => res.json());
+		console.log('Fetched databases', result);
 	})
 
 	vscode.commands.registerCommand('astra-vscode.refreshEntry', () => {
